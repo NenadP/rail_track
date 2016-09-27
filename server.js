1bdc10dea240e2502153198550bdd19483660199
Nenad Pantic <nenopantic@gmail.com>
2016-09-28 00:01:12 +0100

    Experiment integrating mongoose'

--- server.js
+++ server.js
@@ -6,42 +6,78 @@ const parseString = require('xml2js').parseString;
 
 // Create a server with a host and port
 const server = new Hapi.Server();
+
 server.connection({
 	host: 'localhost',
 	port: 8000
 });
 
-// Add the route
-server.route({
-	method: 'GET',
-	path: '/current-trains',
-	handler: function (request, reply) {
-		const options = {
-			host: 'api.irishrail.ie',
-			path: '/realtime/realtime.asmx/getCurrentTrainsXML'
-		};
+const options = {
+	bluebird: false,
+	uri: 'mongodb://localhost:27017'
+};
 
-		http.get(options, function (response) {
-			let xml = '';
+server.register({
+	register: require('hapi-mongoose'),
+	options: options
+}, (err) => {
+	// Add the route
+	server.route({
+		method: 'GET',
+		path: '/current-trains',
+		handler: function (request, reply) {
+			const options = {
+				host: 'api.irishrail.ie',
+				path: '/realtime/realtime.asmx/getCurrentTrainsXML'
+			};
 
-			response.on('data', (chunk) => {
-				xml += chunk;
+			http.get(options, function (response) {
+				let xml = '';
+
+				response.on('data', (chunk) => {
+					xml += chunk;
+				});
+
+				response.on('end', () => {
+					parseString(xml, (error, result) => reply({message: result}));
+				});
+
+			}).on("error", (error) => {
+				console.log("Got error: " + error.message);
 			});
-
-			response.on('end', () => {
-				parseString(xml, (error, result) => reply({message: result}));
-			});
-
-		}).on("error", (error) => {
-			console.log("Got error: " + error.message);
-		});
-	}
-});
+		}
+	});
 
 // Start the server
-server.start((err) => {
-	if (err) {
-		throw err;
-	}
-	console.log('Server running at:', server.info.uri);
+	server.start((err) => {
+		if (err) {
+			throw err;
+		}
+		console.log('Server running at:', server.info.uri);
+
+		const db = server.plugins['hapi-mongoose'].connection; // Get the current connection for this server instance
+		const mongoose = server.plugins['hapi-mongoose'].lib;
+		const Schema = mongoose.Schema;
+
+		var trainSchema = new Schema({
+			//TODO
+		});
+
+		var Train = db.model('Train', trainSchema);
+
+		var small = new Train({ size: 'small' });
+
+		small.save(function (err) {
+			if (err) {
+				console.warn(err);
+			}
+			// saved!
+		});
+
+
+
+	});
+
 });
+
+
